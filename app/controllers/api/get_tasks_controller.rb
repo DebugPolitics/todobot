7fa47da8d478e1ea6b32a5cc5dbbbce23ab3730c
Miguel Carranza <miguel@miguelcarranza.es>
2016-12-19 23:23:20 -0800

    basic get task endpoint

--- app/controllers/api/get_tasks_controller.rb
+++ app/controllers/api/get_tasks_controller.rb
@@ -1,7 +1,19 @@
 class Api::GetTasksController < Api::ApiController
   before_filter :verify_slack_token!
+
   def create
-    render json: { text: 'Hello world!' }, status: :ok
+    user = User.find_or_initialize_by(slack_id: params[:user_id])
+    user.name = params[:user_name]
+    user.save!
+
+    task = user.get_random_task
+
+    if task.nil?
+      render json: { text: 'There are no more tasks for you! Please try again later' },
+             status: :ok
+    else
+      render json: { text: task.description, status: :ok }
+    end
   end
 
   private
