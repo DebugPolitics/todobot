7474fa10d830ab8cc7ce5dab3a2468cde1332a08
Miguel Carranza <miguel@Miguels-MBP.hsd1.ca.comcast.net>
2016-12-19 23:13:45 -0800

    get random task

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :slack_id, presence: true, uniqueness: true
+
+  def get_random_task
+    already_completed_task_ids = self.tasks.where(is_multi_use: false).pluck(:id)
+    candidate_tasks = Task.where(has_expired: false)
+                          .where.not(id: already_completed_task_ids)
+    candidate_tasks.sample
+  end
 end
