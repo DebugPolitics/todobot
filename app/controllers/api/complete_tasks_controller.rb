bd1267e3fa6956627b3bfd402902bc5bcac49c40
Miguel Carranza <miguel@miguelcarranza.es>
2016-12-20 00:35:49 -0800

    completing tasks

--- app/controllers/api/complete_tasks_controller.rb
+++ app/controllers/api/complete_tasks_controller.rb
@@ -1,10 +1,29 @@
 class Api::CompleteTasksController < Api::ApiController
   def create
-    # user = User.find_by_slack_id(params[:user][:id])
-    # task = Task.find_by_id(params[:actions].first[:value])
+    parsed_payload = JSON.parse(params[:payload])
+    user = User.find_by_slack_id(parsed_payload['user']['id'])
+    task = Task.find_by_id(parsed_payload['actions'].first['value'])
 
-    render json: { response_type: 'ephemeral', replace_original: true,
-                   text: "Sorry, that didn't work. Please try again."
-    }, status: :ok
+    if user.nil? or task.nil?
+      render json: error_response, status: :ok
+    else
+      user.tasks << task
+      render json: success_response, status: :ok
+    end
+  end
+
+  private
+  def base_response(text)
+    { response_type: 'ephemeral', replace_original: true,
+      text: text
+    }
+  end
+
+  def error_response
+    base_response("Sorry, that didn't work. Please try again.")
+  end
+
+  def success_response
+    base_response("Thanks for completing the task!")
   end
 end
