4e27d43234a6afd32f7eae3a1525b861a0413a8d
Avand Amiri <avand@avandamiri.com>
2017-01-03 19:39:35 -0800

    Changing output for /leaderboard to include count

--- app/controllers/api/leaderboard_controller.rb
+++ app/controllers/api/leaderboard_controller.rb
@@ -1,13 +1,19 @@
 class Api::LeaderboardController < Api::ApiController
 
   def create
-    user_names_and_scores = User.all.map { |u| [u.name, u.tasks.sum(:bounty)] }
-    user_names_and_scores.sort! { |a, b| b[1] <=> a[1] }
+    users = User.all.map { |u| [u.name, u.tasks.sum(:bounty), u.tasks.count] }
+    users.sort! { |a, b| b[1] <=> a[1] }
 
     text = "Here are your leaders...\n"
 
-    user_names_and_scores.each_with_index do |user_name_and_score, i|
-      text = "#{text}\n#{i + 1}. #{user_name_and_score[0]} (#{user_name_and_score[1]})"
+    users.each_with_index do |user, i|
+      name = user[0]
+      points = user[1]
+      count = user[2]
+
+      text = "#{text}\n#{i + 1}. #{name} " +
+        "(#{points} #{"point".pluralize(points)}, " +
+        "#{count} #{"tasks".pluralize(count)})"
     end
 
     render json: { text: text }, status: :ok
