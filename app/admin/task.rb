6604707f01fbe3994fa82bf8e5e1e1d5c46aec21
Robert Diamond <rmd6502@gmail.com>
2017-01-22 10:47:26 -0800

    figured it out

--- app/admin/task.rb
+++ app/admin/task.rb
@@ -3,17 +3,32 @@ ActiveAdmin.register Task do
   filter :is_multi_use
   filter :categories
 
-  permit_params :description, :is_multi_use, :has_expired, :bounty, category_ids: [:name]
+  permit_params :description, :is_multi_use, :has_expired, :bounty, category_ids: []
 
   actions :all, except: [:destroy]
 
-  # form do |f|
-  #   f.semantic_errors
-  #   f.input :description
-  #   f.input :categories, :as => :check_boxes, :collection => Category.all
-  #   f.input :is_multi_use
-  #   f.input :has_expired
-  #   f.input :bounty
-  #   f.actions
-  # end
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :description
+      f.input :is_multi_use
+      f.input :has_expired
+      f.input :bounty
+      f.input :categories, as: :check_boxes, checked: Category.pluck(&:id)
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :description
+      row :is_multi_use
+      row :has_expired
+      row :bounty
+      table_for task.categories do |cat|
+        column('Categories', :name)
+      end
+    end
+  end
 end
